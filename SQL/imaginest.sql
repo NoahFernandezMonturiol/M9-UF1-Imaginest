392f40380767fa485459f94e68587a75211fa004
NoahFernandezMonturiol <45206822+NoahFernandezMonturiol@users.noreply.github.com>
2021-03-19 08:39:40 +0100

    Add files via upload

--- SQL/imaginest.sql
+++ SQL/imaginest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-03-2021 a las 12:20:24
+-- Tiempo de generación: 19-03-2021 a las 08:39:19
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.4.14
 
@@ -20,8 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `imaginest`
 --
-CREATE DATABASE IF NOT EXISTS `imaginest` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-USE `imaginest`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imatges`
+--
+
+CREATE TABLE `imatges` (
+  `idImatge` int(11) NOT NULL,
+  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `dataPujada` datetime NOT NULL,
+  `estat` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `imatges`
+--
+
+INSERT INTO `imatges` (`idImatge`, `filename`, `dataPujada`, `estat`) VALUES
+(1, 'noah.fnndz_2020_05_27_13_14_07.jpg', '2021-03-12 09:41:34', '1'),
+(2, '1366_1.jpg', '2021-03-12 09:54:31', '1'),
+(3, '1366_1.jpg', '2021-03-12 10:17:36', '1'),
+(4, 'asus.png', '2021-03-12 12:06:40', '1');
 
 -- --------------------------------------------------------
 
@@ -29,7 +50,6 @@ USE `imaginest`;
 -- Estructura de tabla para la tabla `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `iduser` int(11) NOT NULL,
   `mail` varchar(40) COLLATE utf8mb4_bin DEFAULT NULL,
@@ -52,10 +72,19 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
+INSERT INTO `users` (`iduser`, `mail`, `username`, `passHash`, `userFirstName`, `userLastName`, `creationDate`, `lastSignIn`, `removeDate`, `active`, `activationDate`, `activationCode`, `resetPass`, `resetPassExpiry`, `resetPassCode`) VALUES
+(64, 'slastic11@gmail.com', 'noah', '$2y$10$SW.zBqwnrULWrd5NQVf8LeBpcGEEqLCuf1QAwQ6KgFhHVnaLytnG.', 'Noah', 'Fernández', '2021-03-12 10:47:18', '2021-03-12 10:47:58', NULL, 1, NULL, 'da07e2d0f18aee69b6f9c3195d19b970bac5b2c9e525977c24a1c22508f10fde', 1, '2021-03-16 19:29:26', '8bf79da2dbf9cfc40d9d4e03e4d94d00bc6e99ba954feffcbe90b74eba8a0a23'),
+(66, 'noah.fernndez@gmail.com', 'martinet', '$2y$10$WrQccPZmvXpmp2zoHDqsCeJZ0joODrgPvmhPoKpJVk2bQyfT763EK', 'Marti', 'Espinar', '2021-03-19 08:29:44', '2021-03-19 08:30:07', NULL, 1, NULL, '6b9752440ad18015362acde0b48907c8a389b066e310893f7a47488d33c434c7', 1, '2021-03-19 10:00:18', 'd5eb28217d54565078dc2774843bcd9a9698e5780c7c13f9f04ea16b30a7163b');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `imatges`
+--
+ALTER TABLE `imatges`
+  ADD PRIMARY KEY (`idImatge`);
 
 --
 -- Indices de la tabla `users`
@@ -70,16 +99,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `imatges`
+--
+ALTER TABLE `imatges`
+  MODIFY `idImatge` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 
 --
 -- Metadatos
 --
 USE `phpmyadmin`;
+
+--
+-- Metadatos para la tabla imatges
+--
 
 --
 -- Metadatos para la tabla users
